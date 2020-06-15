829e303c56fb36f27119c341a826bfdb51fa83e6
Matthew M <mbmoorin@gmail.com>
2020-06-15 20:17:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,6 +19,15 @@ function whileLoop(number) {
   return "done";
 }
 
+let i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(integer) {
-  
+  do {
+    console.log("I run once regardless.");
+  } while(incrementVariable() < num) 
 }
